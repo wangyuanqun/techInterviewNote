d80de9714da1d3e2955b913f305caefa41ca9323
Yuanqun Wang <1510365904bb@gmail.com>
2026-01-28 11:58:41 +1100

    add file

--- main/C++/Test/test.cpp
+++ main/C++/Test/test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 int main() {
+    // You need to use an ifstream if you just want to read (use an ofstream to write, or an fstream for both).
     std::cout << "what is your name and age: ";
-    int age = 0;
-    std::string name = "";
+    int age;
+    std::string name;
     std::cin >> name >> age;
 
     std::cout << "Nice to meet you " << name << ", and you are " << age << " years old." << std::endl;
